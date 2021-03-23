fe20030ce9c9ca2ffe5602c579c0a84af4249f51
Rick Bischoff <rdbisch@gmail.com>
2021-03-23 14:00:30 -0500

    remove LCD stuff

--- exteral_code/rfm9x_check.py
+++ exteral_code/rfm9x_check.py
@@ -12,33 +12,36 @@ import board
 import adafruit_ssd1306
 # Import the RFM9x radio module.
 import adafruit_rfm9x
- 
+
+# rdb4/rdbisch  Lots of code here is for an LCD display
+#  that I do not have. So I am commenting it out.
+
 # Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
+#btnA = DigitalInOut(board.D5)
+#btnA.direction = Direction.INPUT
+#btnA.pull = Pull.UP
  
 # Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
+#btnB = DigitalInOut(board.D6)
+#btnB.direction = Direction.INPUT
+#btnB.pull = Pull.UP
  
 # Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
+#btnC = DigitalInOut(board.D12)
+#btnC.direction = Direction.INPUT
+#btnC.pull = Pull.UP
  
 # Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
  
 # 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+#reset_pin = DigitalInOut(board.D4)
+#display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 # Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
+#display.fill(0)
+#display.show()
+#width = display.width
+#height = display.height
  
 # Configure RFM9x LoRa Radio
 CS = DigitalInOut(board.CE1)
@@ -47,34 +50,34 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
  
 while True:
     # Clear the image
-    display.fill(0)
+    #display.fill(0)
  
     # Attempt to set up the RFM9x Module
     try:
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-        display.text('RFM9x: Detected', 0, 0, 1)
+        print('RFM9x: Detected')
     except RuntimeError as error:
         # Thrown on version mismatch
-        display.text('RFM9x: ERROR', 0, 0, 1)
+        #display.text('RFM9x: ERROR', 0, 0, 1)
         print('RFM9x Error: ', error)
  
     # Check buttons
-    if not btnA.value:
+    #if not btnA.value:
         # Button A Pressed
-        display.text('Ada', width-85, height-7, 1)
-        display.show()
-        time.sleep(0.1)
-    if not btnB.value:
-        # Button B Pressed
-        display.text('Fruit', width-75, height-7, 1)
-        display.show()
-        time.sleep(0.1)
-    if not btnC.value:
-        # Button C Pressed
-        display.text('Radio', width-65, height-7, 1)
-        display.show()
-        time.sleep(0.1)
+    #    display.text('Ada', width-85, height-7, 1)
+    #    display.show()
+    #    time.sleep(0.1)
+    #if not btnB.value:
+    #    # Button B Pressed
+    #    display.text('Fruit', width-75, height-7, 1)
+    #    display.show()
+    #    time.sleep(0.1)
+    #if not btnC.value:
+    #    # Button C Pressed
+    #    display.text('Radio', width-65, height-7, 1)
+    #    display.show()
+    #    time.sleep(0.1)
  
-    display.show()
+    #display.show()
     time.sleep(0.1)
 
