273435689594429c7b84f87ea6c5ecf16837a008
Rick Bischoff <rdbisch@gmail.com>
2021-04-10 19:20:29 -0500

    adding pi lora radio.

---
+++ exteral_code/radio_rfm9x.py
@@ -0,0 +1,36 @@
+"""
+Example for using the RFM9x Radio with Raspberry Pi.
+
+Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
+Author: Brent Rubell for Adafruit Industries
+"""
+# Import Python System Libraries
+import time
+# Import Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# Import RFM9x
+import adafruit_rfm9x
+
+# Configure LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x.tx_power = 23
+prev_packet = None
+
+while True:
+    packet = None
+    # check for packet rx
+    packet = rfm9x.receive()
+    if packet is None:
+        print("- Waiting for PKT -")
+    else:
+        # Display the packet text and rssi
+        #print(str(packet, "utf-8"))
+        print(packet)
+        time.sleep(1)
+
+    time.sleep(0.1)
