e77156af6cee6374f7dd55ad8527f382a50f4396
Rick Bischoff <rdbisch@gmail.com>
2021-04-29 10:19:44 -0500

    syncing arduino dev pc

---
+++ arduino/driver/driver.ino
@@ -0,0 +1,134 @@
+/* driver.ino
+ * rdb4 4/10/2021
+ *
+ * This is the main driver code for the floor register hardware.
+ * It is based off sample code available in
+ * external_code/lora_test.ino
+ * external_code/pct2075_test.ino
+ * and
+ * Servo demo code sweep
+ */
+
+#include <Adafruit_PCT2075.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+#include "LowPower.h"
+
+/* for feather32u4 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+ 
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+Adafruit_PCT2075 PCT2075;
+
+ 
+void setup() {
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(1);
+    }
+
+    delay(100);
+
+    Serial.println("Feather LoRa TX Test!");
+
+    // manual reset
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
+
+    while (!rf95.init()) {
+        Serial.println("LoRa radio init failed");
+        Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+        while (1);
+    }
+    Serial.println("LoRa radio init OK!");
+
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+    if (!rf95.setFrequency(RF95_FREQ)) {
+        Serial.println("setFrequency failed");
+        while (1);
+    }
+    Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+    // The default transmitter power is 13dBm, using PA_BOOST.
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+    // you can set transmitter powers from 5 to 23 dBm:
+    rf95.setTxPower(23, false);
+
+
+    /******* PCT2075 Setup **********/
+    PCT2075 = Adafruit_PCT2075();
+
+    while (1) {
+        if (!PCT2075.begin()) {
+            Serial.println("Couldn't find PCT2075 chip");
+            delay(1000);
+        } else {
+            Serial.println("Fantastic!");
+            break;
+        }
+    }
+    Serial.println("Found PCT2075 chip");
+}
+ 
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+ 
+void loop()
+{
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+
+  float f = PCT2075.getTemperature();
+  char radiopacket[30];
+  sprintf(radiopacket, "Packet %d Temp %d.%02d", packetnum++, (int)f, (int)(f*100)%100);
+
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[29] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
+ 
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+ 
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+  Serial.println("Going to sleep for 8 seconds.");
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
+        TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_OFF);}
