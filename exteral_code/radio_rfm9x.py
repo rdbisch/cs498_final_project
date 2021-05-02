ddbfea58faf73d84b99ed17ea574ada72e924112
Rick Bischoff <rdbisch@gmail.com>
2021-05-02 06:28:39 -0500

    added  DS18B20 code to pi

--- exteral_code/radio_rfm9x.py
+++ exteral_code/radio_rfm9x.py
@@ -12,6 +12,7 @@ from digitalio import DigitalInOut, Direction, Pull
 import board
 # Import RFM9x
 import adafruit_rfm9x
+import glob
 
 # Configure LoRa Radio
 CS = DigitalInOut(board.CE1)
@@ -21,12 +22,41 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 23
 prev_packet = None
 
+# From https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/software
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+#class Thermostat:
+#    def __init__(self):
+#        self.nodes = { 2**i : None for i in range(1, 32) }
+#        self.
+
 tempSum = 0
 tempCount = 0
 tempSumSq = 0
 badPackets = 0
 
 while True:
+    print("Temp at thermostat is {}".format(read_temp()))
     packet = None
     # check for packet rx
     packet = rfm9x.receive(with_header=True)
